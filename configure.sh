8da38afa3f598b39436b39c58e36b771e7e915f9
xiao <zxinmyth@gmail.com>
2019-11-28 09:30:24 +0800

    configure

--- configure.sh
+++ configure.sh
@@ -1,8 +1,27 @@
+PACK_MAN=""
+if [ -n "$(command -v apt)" ]
+then
+  PACK_MAN="apt"
+elif [ -n "$(command -v yum)" ]
+then
+  PACK_MAN="yum"
+fi
+if [ -z "$PACK_MAN" ]
+then
+  echo "No package manager was found! make sure python3 and cmake are available on the system!"
+  read -n 1 -p "(y/Y)" CHOICE
+  if [ "$CHOICE" != "y" ] && [ "$CHOICE" != "Y" ]
+  then
+    exit 0
+  fi
+fi
+sudo $PACK_MAN install python -y
+
 git submodule update --init --recursive
 cd .vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.py --clang-completer --ts-completer
 cd -
-rsync -av --delete .bashrc $HOME/.bashrc
-rsync -av --delete .vimrc $HOME/.vimrc
-rsync -av --delete .vim/ $HOME/.vim
+rsync -qav --delete .bashrc $HOME/.bashrc
+rsync -qav --delete .vimrc $HOME/.vimrc
+rsync -qav --delete .vim/ $HOME/.vim
