f3a8df9f4199778834a9ba78778f1fa74e786da6
xiao <zxinmyth@gmail.com>
2019-11-28 10:14:50 +0800

    done configuration

--- configure.sh
+++ configure.sh
@@ -1,11 +1,15 @@
 PACK_MAN=""
+PACKAGES="python3 cmake gcc make npm"
 if [ -n "$(command -v apt)" ]
 then
   PACK_MAN="apt"
+  PACKAGES=$PACKAGES:" python3-dev g++"
 elif [ -n "$(command -v yum)" ]
 then
+  PACKAGES=$PACKAGES" python3-devel gcc-c++"
   PACK_MAN="yum"
 fi
+echo $PACKAGES
 if [ -z "$PACK_MAN" ]
 then
   echo "No package manager was found! make sure python3 and cmake are available on the system!"
@@ -15,12 +19,12 @@ then
     exit 0
   fi
 fi
-sudo $PACK_MAN install python -y
+sudo $PACK_MAN install $PACKAGES -y
 
 git submodule update --init --recursive
 cd .vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-./install.py --clang-completer --ts-completer
+python3 install.py --clang-completer --ts-completer
 cd -
 rsync -qav --delete .bashrc $HOME/.bashrc
 rsync -qav --delete .vimrc $HOME/.vimrc
