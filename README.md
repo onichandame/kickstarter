467f16d6c66bd0c00e7d756d0338f7a6da6cd8f9
Zhang Xiao <zhangxiao@neuboron.com>
2019-12-10 18:00:03 +0800

    reworking

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # Home Directory
 
-This repo contains basic configuration files for a Linux home directory. This repo is intended to be used only once right after a fresh installation.
+This repo contains basic configuration files for a Linux home directory.
+
+This script basically installs several essential tools using your system's default package manager and installs vim from source. details of vim installation can be found at [README.md](https://github.com/onichandame/vim-configure/README.md)
 
 # Author
 
 Xiao Zhang
 
+# Pre-requisite
+
+bash, apt/yum
+
 # Usage
 
 1. clone this repo to your home directory. Normally it is **$HOME**.
 2. make sure you have backed up *.bashrc*, *.vimrc* and *.vim/* before proceeding
-3. read *configure.sh* before proceeding as it auto-manages some packages and files.
-4. run *configure.sh*
+3. read *configure.sh* before proceeding as you should never trust a script before reading it
+4. run *.\/configure.sh* with arguments
 
+# Configuration
+
+- **--desktop** Default. Enables all defaults and adds g++, python3, vim and desktop vim configuration
+- **--server** If specified, enables all defaults and adds g++, python, vim and server vim configuration
+- **--default** If specified without *--desktop* or *--server*, enables only cmake, gcc and make
+- **--no-cmake** If specified, don't try to install cmake
+- **--no-make** If specified, don't try to install make
+- **--no-gcc** If specified, don't try to install gcc
+- **--g++** If specified without *--no-gcc*, enables g++
+- **--python** If specified enables python2
+- **--python3** If specified enables python3
+- **--vim** If specified installs vim
+- **--fancy-vim** If specified installs vim and configures vim for normal desktop user
+
+note1: if both *desktop* and *server* are specified, only the former one takes effect.
+
+note2: if a meta option conflicts with a basic option, the basic option overloads the meta one. e.g. *desktop* and *no-gcc* are both specified, all *desktop* options will be enabled but gcc.
--- configure.sh
+++ configure.sh
@@ -1,45 +1,118 @@
+#!/usr/bin/env bash
+
+declare -A CONF
+CONF[PYTHON]=false
+CONF[PYTHON3]=true
+CONF[VIM]=true
+CONF[FANCY_VIM]=true
+CONF[CXX]=true
+
 PACK_MAN=""
-PACKAGES="vim python3 cmake gcc make nodejs"
-if [ -n "$(command -v apt)" ]
-then
-  PACK_MAN="apt"
-  PACKAGES=$PACKAGES:" python3-dev g++"
-elif [ -n "$(command -v yum)" ]
-then
-  curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-  PACKAGES=$PACKAGES" python3-devel gcc-c++"
-  PACK_MAN="yum"
-fi
-echo $PACKAGES
-if [ -z "$PACK_MAN" ]
-then
-  echo "No package manager was found! make sure python3 and cmake are available on the system!"
-  read -n 1 -p "(y/Y)" CHOICE
+PACKAGES=("cmake" "gcc" "make")
+
+main(){
+
+  declare -A ARGS
+  ARGS[DESKTOP]=
+  ARGS[SERVER]=
+  ARGS[DEFAULT]=
+  ARGS[CMAKE]=
+  ARGS[MAKE]=
+  ARGS[GCC]=
+  ARGS[GXX]=
+  ARGS[PYTHON]=
+  ARGS[PYTHON3]=
+  ARGS[VIM]=
+  ARGS[FANCY_VIM]=
+
+  for PARAM in "$@"
+  do
+    if [ "$PARAM" = "--desktop" ]
+    then
+      if [ -z "${ARGS[DESKTOP]}" ]
+      then
+        ARGS[DESKTOP]=true
+        ARGS[SERVER]=false
+        ARGS[DEFAULT]=true
+      fi
+    elif [ "$PARAM" = "--server" ]
+      if [ -z "${ARGS[SERVER]}"]
+      then
+        ARGS[DESKTOP]=false
+        ARGS[SERVER]=true
+        ARGS[DEFAULT]=true
+      fi
+    elif [ "$PARAM" = "--default" ]
+      if [ -z "${ARGS[DEFAULT]}" ]
+      then
+        ARGS[DEFAULT]=true
+        ARGS[DESKTOP]=false
+        ARGS[SERVER]=false
+      fi
+    elif [ "$PARAM" = "--no-cmake" ]
+      if [ -z "${ARGS[CMAKE]}" ]
+      then
+        ARGS[CMAKE]=false
+      fi
+    fi
+  done
+
+  exit 0
+  
+  # Comprehend list of packages
+  if [ -n "$(command -v apt)" ]
+  then
+    PACK_MAN="apt"
+    PACKAGES=$PACKAGES" python3-dev g++"
+  elif [ -n "$(command -v yum)" ]
+  then
+  #  curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+    PACKAGES=$PACKAGES" python3-devel gcc-c++"
+    PACK_MAN="yum"
+  fi
+  
+  # Install packages
+  if [ -z "$PACK_MAN" ]
+  then
+    echo "No package manager was found! ARE YOU SURE that python3.3+ development headers, python3.3+ and cmake are available on the system?"
+    read -n 1 -p "(Y/y)" CHOICE
+    if [ "$CHOICE" != "y" ] && [ "$CHOICE" != "Y" ]
+    then
+      exit 0
+    fi
+  else
+    install_pkg
+  fi
+  
+  # Install vim from source
+  install_vim
+  
+  # Overwrite .bashrc
+  echo "packages were installed! do you wish to overwrite .bashrc?"
+  read -n 1 -p "(Y/y)" CHOICE
   if [ "$CHOICE" != "y" ] && [ "$CHOICE" != "Y" ]
   then
     exit 0
+  else
+    cp -f .bashrc $HOME/.bashrc
   fi
-fi
-sudo $PACK_MAN install $PACKAGES -y
-VI="$(which vi)"
-VIM="$(which vim)"
-if [ -n $VI ]
-then
-  sudo rm $VI
-else
-  VI="/usr/bin/vi"
-fi
-if [ -z $VIM ]
-then
-  VIM="/usr/bin/vim"
-fi
-sudo ln -s $VIM $VI
+}
 
-git submodule update --init --recursive
-cd .vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-python3 install.py --clang-completer --ts-completer
-cd -
-rsync -qav --delete .bashrc $HOME/.bashrc
-rsync -qav --delete .vimrc $HOME/.vimrc
-rsync -qav --delete .vim/ $HOME/.vim
+install_pkg () {
+  sudo $PACK_MAN install $PACKAGES -y
+}
+
+install_vim () {
+  git submodule update --init vim
+  cd vim
+  python3 configure.py
+  if [ $? -ne 0 ]
+  then
+    cd -
+    echo "Vim installation failed. Try debugging it or contact the author"
+    exit 1
+  fi
+  cd -
+}
+
+main "$@"; exit
